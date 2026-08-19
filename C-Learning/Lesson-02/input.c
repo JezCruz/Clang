0cfc7732ad58cbdd0c3c9bb301ff8f6872003de6
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-08-19 14:59:28 +0800

    Input - height

--- C-Learning/Lesson-02/input.c
+++ C-Learning/Lesson-02/input.c
@@ -9,5 +9,12 @@ int main(void)
     
     printf("Your age is %d\n", age);
 
+    float height;
+
+    printf("Enter your height: ");
+    scanf("%f", &height);
+
+    printf("Your height is %.1f\n", height);
+    
     return 0;
 }
